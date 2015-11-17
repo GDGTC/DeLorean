254ffa2752461858260838f4e6492add61007408
Stephen Fluin <stephen.fluin@gmail.com>
2015-11-16 22:36:04 -0600

    Working on date formatting and added jpgs to dev serve

--- Gruntfile.js
+++ Gruntfile.js
@@ -78,7 +78,7 @@ module.exports = function (grunt) {
           open: true,
           middleware: function (connect) {
             return [
-              modRewrite(['!\\.html|\\.js|\\.svg|\\.css|\\.png$ /index.html [L]']),
+              modRewrite(['!\\.html|\\.js|\\.svg|\\.css|\\.jpg|\\.png$ /index.html [L]']),
               connect.static('.tmp'),
               connect().use(
                 '/bower_components',
