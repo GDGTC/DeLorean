7981233281fa1cc807899362881742a2700eaa7a
Stephen Fluin <stephen.fluin@gmail.com>
2015-12-22 00:19:40 -0600

    Improvements to speaker assignments on schedule. Rerendered schedule for admins only

--- app/scripts/controllers/schedule-edit.js
+++ app/scripts/controllers/schedule-edit.js
@@ -8,8 +8,23 @@
  * Controller of the devfestApp
  */
 angular.module('devfestApp')
-  .controller('ScheduleEditCtrl', function ($scope, Ref, $firebaseArray, $timeout, $modal, $window, $location, Config) {
+  .controller('ScheduleEditCtrl', function ($scope, Ref, $firebaseArray, $firebaseObject, $timeout, $modal, $window, $location, Config) {
     $scope.schedule = $firebaseArray(Ref.child('devfest2016').child('schedule'));
     $scope.speakers = $firebaseArray(Ref.child('devfest2016').child('speakers'));
+    $scope.speakersAsObject = $firebaseObject(Ref.child('devfest2016').child('speakers'));
+
+    $scope.addSpeaker = function(session, speakerId) {
+    	var sessionSpeakers = $firebaseArray($scope.schedule.$ref().child(session.$id).child('speakers'));
+    	sessionSpeakers.$add(speakerId);
+    };
+
+    $scope.rooms = {
+	"auditorium":"Main Auditorium",
+	"smallauditorium": "Small Auditorium",
+	"lab": "Laboratory Classroom",
+	"classroom1": "Classroom 1",
+	"classroom2": "Classroom 2",
+	"classroom3": "Classroom 3",
+	"cafeteria": "Cafeteria"};
     
 });
--- app/scripts/controllers/schedule.js
+++ app/scripts/controllers/schedule.js
@@ -6,22 +6,9 @@ var rooms = {
 	"lab": "Laboratory Classroom",
 	"classroom1": "Classroom 1",
 	"classroom2": "Classroom 2",
-	"caffeteria": "Caffeteria"};
+	"classroom3": "Classroom 3"};
 var roomOrder = Object.keys(rooms);
 
-var times = {
-	0:"8AM",
-	1:"9AM",
-	2:"10AM",
-	3:"11AM",
-	4:"12PM",
-	5:"1PM",
-	6:"2PM",
-	7:"3PM",
-	8:"4PM",
-	9:"5PM"
-};
-var timesOrder = Object.keys(times);
 
 /**
  * @ngdoc function
@@ -31,34 +18,67 @@ var timesOrder = Object.keys(times);
  * Controller of the devfestApp
  */
 angular.module('devfestApp')
-  .controller('ScheduleCtrl', function ($scope, Ref, $firebaseArray, $timeout, $modal, $window, $location, Config) {
-    $scope.schedule = $firebaseArray(Ref.child('schedule'));
+  .controller('ScheduleCtrl', function ($scope, Ref, $firebaseArray, $firebaseObject, $timeout, $modal, $window, $location, Config) {
+    $scope.schedule = $firebaseArray(Ref.child('devfest2016').child('schedule'));
+    $scope.speakersAsObject = $firebaseObject(Ref.child('devfest2016').child('speakers'));
     $scope.rooms = rooms;
     $scope.roomOrder = roomOrder;
-    $scope.times = times;
+    //$scope.times = times;
 //    $scope.timeslottimes = [];
 
+	// I hate timezones. - Stephen 2015-12-22 11:48 GMT -6 
 
-    
+    // Timezones suck. All DB dates are in UTC
+    var everythingStart = new Date(Date.UTC(2016,2,6,14,0,0));
+
+
 	var unwatch = $scope.schedule.$watch(function() {
   		console.log("data changed!");
   		console.log($scope.schedule);
-  		var startTimes = [];
 
-		for(var t=0;t<timesOrder.length;t++){
-			startTimes[t] = [];
-			for(var r=0;r<roomOrder.length;r++){
-				startTimes[t][r] = 0;		
-			}
-		}
-  		for(var i = 0;i<$scope.schedule.length;i++) {
+  		var p, prettySchedule;
+
+
+  		// First let's make an array of objects for our time slots
+  		p = {};
+  		for (var i = 8; i <= 17; i++) {
+  			console.log("intializing " + (i) + "h");
+  			p[i] = {all:[]};
+  			for (var room in rooms) {
+  				p[i][room] = [];
+  			};
+  		};
+
+  		// Now let's put all of our schedule items into their time slots
+  		for (var i = $scope.schedule.length - 1; i >= 0; i--) {
   			var session = $scope.schedule[i];
-  			var ro = roomOrder.indexOf(session.room);
-  			startTimes[session.timeslot][ro] = session;
-  		}
-  		console.log("Finished processing, generated:",startTimes);
-  		console.log("Timeslottimes", times);
-  		//TODO: startTimes.sort();
-  		$scope.timeSlots = startTimes;
+  			var hour = new Date(session.startTime).getUTCHours();
+  			hour = hour - 6; // Get us back to Central time regardleses of locale;
+
+  			if(session.all) {
+  				p[hour].all.push(session);
+  			} else if(session.room) {
+  				p[hour][session.room].push(session);
+  			} else {
+  				//room not specified and it's not an "all attendee", somebody messed up their data entry!
+  			}
+  		};
+
+  		$scope.prettySchedule = p;
+
 	});
 });
+
+
+/**
+
+Let's make the data look like
+[{UTCTIME8AM:
+	{alls:
+	 [{},{}]
+	 room1: {}
+	 room2: {}
+	}
+}]
+
+*/
